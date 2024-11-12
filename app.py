6dfd58c6d392cdc1d3ec31ab739528800d778111
ali ebrahimi <aliactionsmart3@gmail.com>
2024-11-12 10:28:26 +0330

    seccond-commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+import json
+
+from flask import Flask, render_template, redirect, url_for, Response
 import docker
 
 app = Flask(__name__)
@@ -26,6 +28,19 @@ def delete_container(container_id):
     container = client.containers.get(container_id)
     container.remove(force=True)
     return redirect(url_for('list_containers'))
+
+
+@app.route('/inspect/<container_id>')
+def inspect_container(container_id):
+    container = client.containers.get(container_id)
+    inspection_data = container.attrs  # Fetches the detailed inspection information
+    inspection_text = json.dumps(inspection_data, indent=4)  # Convert JSON data to pretty-printed text
+    return Response(
+        inspection_text,
+        mimetype="text/plain",
+        headers={"Content-Disposition": f"attachment;filename={container_id}_inspect.txt"}
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
